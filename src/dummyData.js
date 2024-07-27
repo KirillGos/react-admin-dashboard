46fd7e6dbca975c4192828aac9406649d336b14a
Kirill Goshin <4kirillgo@gmail.com>
2024-07-27 16:54:40 +0500

    finsihed table ui

--- src/dummyData.js
+++ src/dummyData.js
@@ -48,3 +48,87 @@ export const userData = [
     "Active User": 4000,
   },
 ];
+
+export const userRows = [
+  {
+    id: 1,
+    username: "Jon Snow",
+    avatar:
+      "https://yt3.ggpht.com/yti/ANjgQV-6phaliBvMyMnCHcNM4kNgc9J74FLqhbiLm9Y-l4J_TQ=s88-c-k-c0x00ffffff-no-rj",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 2,
+    username: "Tony Stark",
+    avatar:
+      "https://yt3.ggpht.com/yti/ANjgQV-6phaliBvMyMnCHcNM4kNgc9J74FLqhbiLm9Y-l4J_TQ=s88-c-k-c0x00ffffff-no-rj",
+    email: "starkInt@hotmail.com",
+    status: "active",
+    transaction: "$200.000.00",
+  },
+  {
+    id: 3,
+    username: "Bruce Benner",
+    avatar:
+      "https://yt3.ggpht.com/yti/ANjgQV-6phaliBvMyMnCHcNM4kNgc9J74FLqhbiLm9Y-l4J_TQ=s88-c-k-c0x00ffffff-no-rj",
+    email: "hulk@gmail.com",
+    status: "unactive",
+    transaction: "$0.00",
+  },
+  {
+    id: 4,
+    username: "Paul Alen",
+    avatar:
+      "https://yt3.ggpht.com/yti/ANjgQV-6phaliBvMyMnCHcNM4kNgc9J74FLqhbiLm9Y-l4J_TQ=s88-c-k-c0x00ffffff-no-rj",
+    email: "flash@outlook.com",
+    status: "active",
+    transaction: "$50.00",
+  },
+  {
+    id: 5,
+    username: "Jon Snow",
+    avatar:
+      "https://yt3.ggpht.com/yti/ANjgQV-6phaliBvMyMnCHcNM4kNgc9J74FLqhbiLm9Y-l4J_TQ=s88-c-k-c0x00ffffff-no-rj",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 6,
+    username: "Tony Stark",
+    avatar:
+      "https://yt3.ggpht.com/yti/ANjgQV-6phaliBvMyMnCHcNM4kNgc9J74FLqhbiLm9Y-l4J_TQ=s88-c-k-c0x00ffffff-no-rj",
+    email: "starkInt@hotmail.com",
+    status: "active",
+    transaction: "$200.000.00",
+  },
+  {
+    id: 7,
+    username: "Bruce Benner",
+    avatar:
+      "https://yt3.ggpht.com/yti/ANjgQV-6phaliBvMyMnCHcNM4kNgc9J74FLqhbiLm9Y-l4J_TQ=s88-c-k-c0x00ffffff-no-rj",
+    email: "hulk@gmail.com",
+    status: "unactive",
+    transaction: "$0.00",
+  },
+  {
+    id: 8,
+    username: "Paul Alen",
+    avatar:
+      "https://yt3.ggpht.com/yti/ANjgQV-6phaliBvMyMnCHcNM4kNgc9J74FLqhbiLm9Y-l4J_TQ=s88-c-k-c0x00ffffff-no-rj",
+    email: "flash@outlook.com",
+    status: "active",
+    transaction: "$50.00",
+  },
+  {
+    id: 9,
+    username: "Paul Alen",
+    avatar:
+      "https://yt3.ggpht.com/yti/ANjgQV-6phaliBvMyMnCHcNM4kNgc9J74FLqhbiLm9Y-l4J_TQ=s88-c-k-c0x00ffffff-no-rj",
+    email: "flash@outlook.com",
+    status: "active",
+    transaction: "$50.00",
+  },
+];
